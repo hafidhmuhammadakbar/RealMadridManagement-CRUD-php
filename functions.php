f4d893be381ae12fc64ccb85f9ca3e73354e64dd
hafidhmuhammadakbar <hafidhmuhammadakbar1@student.uns.ac.id>
2023-06-02 12:53:47 +0700

    minimize code

--- functions.php
+++ functions.php
@@ -7,6 +7,75 @@ $nama_database = "realmadrid";
 
 $conn = mysqli_connect($server, $user, $password, $nama_database);
 
+// function header_template
+function header_template($title) {
+    echo <<<EOT
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>$title</title>
+
+        <!-- Css -->
+        <link rel="stylesheet" href="mystyle.css">
+        <!-- Shortcut icon -->
+        <link rel="shortcut icon" href="img/rm-logo.png"/>
+        <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" 
+            integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+        <!-- Font -->
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <header>
+            <!-- navbar -->
+            <nav class="navbar navbar-expand-md fixed-top" id="mainNav">
+                <div class="container">
+                    <a class="navbar-brand" href="index.php">
+                        <img src="img/rm-logo.png" alt="Logo" width="24" height="30" class="d-inline-block align-text-top">
+                        Real Madrid
+                    </a>
+                    <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarCollapse">
+                        <ul class="navbar-nav mb-2 mb-md-0 ms-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.php">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="player.php">Player</a>
+                            </li>
+                        </ul>
+                    </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    EOT;
+}
+
+// template footer
+function footer_template(){
+    echo <<<EOT
+        <footer class="text-center">
+            <a href="https://github.com/hafidhmuhammadakbar" target="_blank">
+                &copy; 2023, Real Madrid Management Player. <br>
+                Hafidh Muhammad Akbar
+            </a>
+        </footer>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" 
+            integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    </body>
+    </html>
+    EOT;
+}
+
 // function query
 function query($query) {
     global $conn;
